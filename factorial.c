7cbb972e970ccbe524306e7f1b9e6812f35d1c0d
chamarthipooja1234-cyber <chamarthipooja1234@gmail.com>
2025-11-29 11:20:59 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+long long fac=1;
+factorial values
+printf("enter a number:");
+scanf("%d",&n);
+if(n<0)
+{
+printf("factorial of negative numbers is not defined.\n");
+}
+else{
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+printf(factorial of %d is %||d\n",fact);
+}
+return 0;
+} 
